fd5da15f7456caa1e5ec4dd47163421deeac36d5
Your Name <abdullahi.abass@student.moringschool.com>
2023-07-02 10:27:40 +0300

    Initial Commit

--- index.js
+++ index.js
@@ -19,3 +19,14 @@ document.addEventListener("DOMContentLoaded", () => {
               });
             });
           }     
+
+          function displayMovie(movie) {
+            const movieName = document.getElementById("name");
+            const movieImage = document.getElementById("poster");
+            const description = document.getElementById("description");
+            const runTime = document.getElementById("runtime");
+            const showTime = document.getElementById("showtime");
+            const capacity = document.getElementById("capacity");
+            const sold = document.getElementById("sold");
+            const available = document.getElementById("available");
+            
